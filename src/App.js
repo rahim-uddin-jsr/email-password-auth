a06f25f1650427f20a9fbfcd0f453e00c306d3c9
dev-rahim <rahimuddin5915@gmail.com>
2023-03-08 08:20:32 +0600

    get userName and Pass from  user input and set in state done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
 
+  const handleEmailPassLogin = (e) => {
+    e.preventDefault()
+    const email = document.getElementById('email').value;
+    setUserEmail(email)
+    const password = document.getElementById('password').value;
+    setUserPassword(userPassword)
+  }
   return (
     <div className="App">
-      <Form className='w-50 mx-auto mt-5'>
+      <Form onSubmit={handleEmailPassLogin} className='w-50 mx-auto mt-5'>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" id="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -16,7 +26,7 @@ function App() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" id="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
